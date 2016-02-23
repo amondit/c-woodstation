9e3f2fe872a39edafb02700ecb02d98977ba0999
Arnaud Mondit <a.mondit@gmail.com>
2016-02-23 16:17:58 -0500

    Update woodstation.c

--- woodstation.c
+++ woodstation.c
@@ -1,18 +1,25 @@
-#include<stdio.h>
-#include<time.h>
+#include "woodstation.h"
 
 int main() {
 
-  time_t rawtime;
-  struct tm * timeinfo;
-  char timeBuff[60];
-  time ( &rawtime );
-  timeinfo = localtime ( &rawtime );
-  if (strftime(timeBuff, sizeof timeBuff, "%H%M%S", timeinfo)) {
-        puts(timeBuff);
-    } else {
-        puts("strftime failed");
-    }
-  printf ( "Current local time and date: %s", asctime (timeinfo) );
-  return 0;
+	time_t displayedRawTime;
+	time ( &displayedRawTime );
+	char timeBuff[60];
+	
+	while(1){
+		time_t rawtime;
+		time ( &rawtime );
+		if (difftime(rawtime, displayedRawTime) != 0) {
+			struct tm * timeinfo;
+			timeinfo = localtime ( &rawtime );
+			if (strftime(timeBuff, sizeof timeBuff, "%H%M%S", timeinfo)) {
+				puts(timeBuff);
+				displayedRawTime = rawtime;
+			} else {
+				puts("strftime failed");
+			}
+		}
+		nanosleep((const struct timespec[]){{0, 50000000L}}, NULL); /* sleep for 0.05s */
+	}
+	return 0;
 }
